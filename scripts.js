718d8c29c5897cfd71f6728c892ec9558f54328d
ayarina <ayat.harastani@gmail.com>
2020-10-30 23:35:17 +0100

    CatAPI Added uwu, scrolltobutton's size modified

--- scripts.js
+++ scripts.js
@@ -6,6 +6,17 @@ function ScrollToTop()
     window.scrollTo({top: 0, behavior: 'smooth'});
 }
 
+//Alerta Viva España
 function Arriba(){
     alert('Vive l\'Espagne');
 }
+
+//API gatitos nekos kawaiis nya~~
+function getNekos(){
+    $.get("https://api.thecatapi.com/v1/images/search", function(data, status){
+        var img = document.getElementById("Neko");
+        img.src = data['0']['url'];
+    });
+}
+
+//Página CatApi: https://docs.thecatapi.com/
